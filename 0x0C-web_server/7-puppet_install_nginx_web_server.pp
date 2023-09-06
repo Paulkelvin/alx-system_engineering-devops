25f59915ae8a866f0bd58c0bce387d7663ef9022
Paulkelvin <ibukunadesanya0@gmail.com>
2023-09-05 17:06:21 -0700

    corrected

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,21 +1,23 @@
 # automating my work using puppet
 
 package { 'nginx':
- ensure => installed,
+  ensure => installed,
 }
 
 file_line { 'install':
- ensure => 'present',
- path   => '/etc/nginx/sites-enabled/default'.
- after  => 'listen 80 default_server;',
- line   => 'rewrite ^redirect_me https://www.github.com/paulkelvin permanent;'.
+  ensure => 'present',
+  path   => '/etc/nginx/sites-enabled/default',
+  after  => 'listen 80 default_server;',
+  line   => 'rewrite ^redirect_me https://www.github.com/paulkelvin permanent;',
 }
 
-file { '/var/www/html/index.html';
- content => 'Hello world!',
- }
-
- service { 'nginx':
-   ensure   => running,
-   require  => package['nginx'],
+file { '/var/www/html/index.html':
+  content => 'Hello world!',
+  # Additional parameters like owner, group, and mode might be needed
 }
+
+service { 'nginx':
+  ensure  => running,
+  require => Package['nginx'],
+}
+
